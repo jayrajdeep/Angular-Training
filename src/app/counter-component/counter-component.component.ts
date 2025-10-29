e5ea52add8a55d5cdc4fe605e32a9c369caf933c
Jaydeepkumar Girase <jaydeepkumar.girase@nihilent.com>
2025-10-29 10:32:15 +0530

    Initial commit

---
+++ src/app/counter-component/counter-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter-component.component.html',
+  styleUrl: './counter-component.component.css'
+})
+export class CounterComponentComponent {
+
+}
